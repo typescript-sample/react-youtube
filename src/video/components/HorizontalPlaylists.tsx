b277c71f1eed977c9785be8841039a225db840f1
thanhhaile051020 <thanhhaile0510@gmail.com>
2022-04-06 11:04:31 +0700

    update list view

--- src/video/components/HorizontalPlaylists.tsx
+++ src/video/components/HorizontalPlaylists.tsx
@@ -9,7 +9,18 @@ export interface Props {
   getPlaylistVideos: (playlistId: string, max?: number, nextPageToken?: string, fields?: string[]) => Promise<ListResult<PlaylistVideo>>;
 }
 const HorizontalPlaylists = (props: Props) => {
+  const scrollRef = React.useRef<any>()
+  const liRef = React.useRef<any>()
   const [playlists, setPlaylists] = React.useState<Playlist[]>([]);
+
+  const scroll = (dir: string) => {
+    const { current } = scrollRef
+    if (dir === 'left') {
+      current.scrollLeft -= liRef.current.offsetWidth
+    } else {
+      current.scrollLeft += liRef.current.offsetWidth
+    }
+  }
 
   React.useEffect(() => {
     (async () => {
@@ -19,18 +30,29 @@ const HorizontalPlaylists = (props: Props) => {
       }
     })();
   }, []);
+
   return (
-    <ul className='list-view horizon'>
-      {playlists && playlists.map((item) => (
-        <li key={item.id} className='video'>
-          <div className='cover'>
-            <Slide id={item.id} thumbnail={item.mediumThumbnail} thumbnailSize='mediumThumbnail' getVideos={props.getPlaylistVideos} />
-          </div>
-          <h4><Link to={`/channels/${item.channelId}`}>{item.title}</Link></h4>
-        </li>
-      )
-      )}
-    </ul>
+    <div className='scroll-view'>
+      <ul className='list-view horizon' ref={scrollRef}>
+        {playlists && playlists.map((item) => (
+          <li key={item.id} className='video' ref={liRef}>
+            <div className='cover'>
+              <Slide id={item.id} thumbnail={item.mediumThumbnail} thumbnailSize='mediumThumbnail' getVideos={props.getPlaylistVideos} />
+            </div>
+            <h4><Link to={`/channels/${item.channelId}`}>{item.title}</Link></h4>
+          </li>
+        )
+        )}
+      
+      </ul>
+      <button type='button' onClick={() => scroll('left')} className='left-arrow'>
+          &#10095;
+        </button>
+        <button type='button' onClick={() => scroll('right')} className='right-arrow'>
+          &#10095;
+        </button>
+    </div>
+
   );
 };
 export default HorizontalPlaylists;
